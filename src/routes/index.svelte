22873a4e01b988d6fa5de4cfda32ab9331d25703
Harald Fauland <harald.fauland@gmail.com>
2022-03-04 22:22:32 +0100

    erste db abfrage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
+<script context="module">
+	import { PrismaClient } from '@prisma/client';
+	const prisma = new PrismaClient();
+
+	export async function load({}) {
+		const kategorien = await prisma.kategorie.findMany();
+		const ausgaben = await prisma.ausgabe.findMany();
+
+		return {
+			props: {
+				kategorien,
+				ausgaben
+			}
+		};
+	}
+</script>
+
 <script>
+	export let kategorien;
+	export let ausgaben;
 </script>
